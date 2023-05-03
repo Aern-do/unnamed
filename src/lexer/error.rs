eabfe9ff2dc0bc00cebb946bb1b24d74171960b7
Raddeonn <radeonchik.new@gmail.com>
2023-05-03 16:00:45 +0300

    feat(lexer): float

--- src/lexer/error.rs
+++ src/lexer/error.rs
@@ -3,11 +3,13 @@ use std::fmt::{self, Display};
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum ErrorKind {
     UnexpectedToken,
+    TooManyFloatingPoints(u8)
 }
 impl Display for ErrorKind {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             ErrorKind::UnexpectedToken => write!(f, "unexpected token"),
+            ErrorKind::TooManyFloatingPoints(points) => write!(f, "Too many decimal points, expected 1, received {points}"),
         }
     }
 }
