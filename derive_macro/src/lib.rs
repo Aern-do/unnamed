cbfa910c1492978f4618c6fa3f43d00b303f16f7
Raddeonn <radeonchik.new@gmail.com>
2023-05-12 23:08:50 +0300

    feat(derive_macro): parse if attribute

--- derive_macro/src/lib.rs
+++ derive_macro/src/lib.rs
@@ -3,7 +3,7 @@ use syn::{parse_macro_input, DeriveInput, Error};
 
 mod parse;
 
-#[proc_macro_derive(Parse)]
+#[proc_macro_derive(Parse, attributes(parse_if))]
 pub fn parse(input: TokenStream) -> TokenStream {
     let derive_input = parse_macro_input!(input as DeriveInput);
     parse::expand(derive_input).unwrap_or_else(Error::into_compile_error).into()
